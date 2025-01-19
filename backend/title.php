8307ed3ae3418c3f9fffe452458bda8677fc38c3
sandy0v0 <be0324ss@gamil.com>
2025-01-19 13:54:59 +0800

    前後台網站系統建置-新增標頭區

--- backend/title.php
+++ backend/title.php
@@ -6,7 +6,7 @@
     <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
         <p class="t cent botli">網站標題管理</p>
         <form method="post" action="./api/edit.php">
-            <table width="100%" >
+            <table width="100%">
                 <tbody>
                     <tr class="yel">
                         <td width="45%">網站標題</td>
@@ -21,10 +21,10 @@
                     ?>
                     <tr>
                         <td>
-                            <img src="./upload/<?=$row['img'];?>" style="width:300px;height:30px;">    
+                            <img src="./upload/<?=$row['img'];?>" style="width:300px;height:30px;">
                         </td>
                         <td>
-                            <input type="text" name="text[]" value="<?=$row['text'];?>">    
+                            <input type="text" name="text[]" value="<?=$row['text'];?>">
                         </td>
                         <td>
                             <input type="radio" name="sh" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
@@ -33,9 +33,9 @@
                             <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
                         </td>
                         <td>
-                            <input type="button" 
+                            <input type="button"
                                 onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/upload_<?=$do;?>.php?id=<?=$row['id'];?>&table=<?=$do;?>&#39;)"
-                                  value="更新圖片">
+                                value="更新圖片">
                         </td>
                         <input type="hidden" name="id[]" value="<?=$row['id'];?>">
                     </tr>
@@ -50,9 +50,10 @@
                         <td width="200px">
                             <input type="button"
                                 onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/<?=$do;?>.php?table=<?=$do;?>&#39;)"
-                                value="新增網站標題圖片"></td>
+                                value="新增網站標題圖片">
+                        </td>
                         <td class="cent">
-                        <input type="hidden" name="table" value="<?=$do;?>">
+                            <input type="hidden" name="table" value="<?=$do;?>">
                             <input type="submit" value="修改確定">
                             <input type="reset" value="重置">
                         </td>
